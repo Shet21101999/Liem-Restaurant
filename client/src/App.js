5df09cd00f089f206ba3e1d0043db585e168f646
LAPTOP-P7N3DN6P\YellowsBlue <lh21101998@gmail.com>
2022-06-10 10:37:44 +0700

    first_commit

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./App.css";
+import {
+  AboutUs,
+  Chef,
+  FindUs,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
+} from "./container";
+import { Navbar } from "./components";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <SpecialMenu />
+      <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
